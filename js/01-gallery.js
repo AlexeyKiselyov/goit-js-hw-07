91acc5d70756764e64a2df4c718c26e0bfe0d1ce
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-08-30 18:10:47 +0300

    Add 1st and 2d task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector(".gallery");
+
+galleryBox.addEventListener("click", onClickModalOpen);
+
+let instance = null;
+
+function onClickModalOpen(e) {
+  e.preventDefault();
+  const dataSrc = e.target.dataset.source;
+  instance = basicLightbox.create(
+    `<img width="1400" height="900" src="${dataSrc}">`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", onKeyEscClose);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", onKeyEscClose);
+      },
+    }
+  );
+  instance.show();
+}
+
+function onKeyEscClose(e) {
+  if (e.code !== "Escape") {
+    return;
+  }
+  instance.close();
+}
+
+function createGalalery(arr) {
+  const galleryStr = arr
+    .map(({ preview, original, description }) => {
+      return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+</div>
+`;
+    })
+    .join("");
+  galleryBox.insertAdjacentHTML("beforeend", galleryStr);
+}
+
+createGalalery(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector(".gallery");
+
+function createGalalery(arr) {
+  const galleryStr = arr
+    .map(({ preview, original, description }) => {
+      return `  
+    <a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"        
+        alt="${description}"        
+      />
+    </a>
+`;
+    })
+    .join("");
+  galleryBox.insertAdjacentHTML("beforeend", galleryStr);
+}
+
+createGalalery(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
